8772eb853da307f4f9d2f742897091a2c7224c80
Luka Mirosevic <luka@goonbee.com>
2013-07-19 21:14:56 +0200

    -n Cleaned old comments

--- messages.rb
+++ messages.rb
@@ -27,28 +27,6 @@ class Hash#foo factor this out into a GBToolbox module
 			result
 		}
 	end
-
-
-	#def symbolize_keys(hash)
-	#	hash.keys.each do |key|
-	#		hash[(key.to_sym rescue key) || key] = hash.delete(key)
-	#	end
-	#end
-	#
-	#def symbolize_keys_deep!
-	#	self.inject({}) do |result, (key, value)|
-	#		new_key = case key
-	#			when String then key.to_sym
-	#			else key
-	#			end
-	#		new_value = case value
-	#			when Hash then symbolize_keys(value)
-	#			else value
-	#			end
-	#		result[new_key] = new_value
-	#		result
-	#	end
-	#end
 end
 
 module Syncable
